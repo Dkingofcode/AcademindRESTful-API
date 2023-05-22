b1d96e74a2a85ee072e32996a4794338c1b858a1
David <doladepo128@gmail.com>
2023-05-22 10:51:58 +0100

    Added file, image upload with multer

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,12 +2,32 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'})
+const upload = multer({storage: storage, limits: {
+   fileSize: 1024 * 1024 * 5
+}});
+
+const storage = multer.diskStorage({
+   destination: function(req, file, cb){
+     cb(null, './uploads/');
+   },
+   filename: function(req, file, cb){
+      cb(null, new Date().toISOString() + file.originalname)
+   }
+})
+
+const fileFilter = (req, file, cb) => {
+   // reject a file
+   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+      cb(null, true);
+   } else {
+      cb(null, false);
+   }
+}
 const Product = require("../models/product");
 
 
 router.get('/', (req, res, next) => {
-    Product.find().select('name price _id').exec().then(docs => {
+    Product.find().select('name price _id productImage').exec().then(docs => {
       const response = {
           count: docs.length,
           products: docs.map(doc => { 
@@ -15,6 +35,7 @@ router.get('/', (req, res, next) => {
                  name: doc.name,
                  price: doc.price,
                  _id: doc._id,
+                 productImage: doc.productImage,
                  request: {
                     type: 'GET',
                     url: 'http://localhost:3000/products/' + doc._id
