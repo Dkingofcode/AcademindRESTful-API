8301453c4d12488ff0abab773e67d1bcd80b68d2
David <doladepo128@gmail.com>
2023-05-26 05:43:47 +0100

    Fixed JWT token error

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-
+const checkAuth = require("../middleware/check-auth")
 const Order = require('../models/order');
 const Product = require('../models/product');
 //import OrdersController from "../controlllers/orders";
+const OrdersController =    require("../controlllers/orders");
+
 
 // Handle incoming GET requests to /orders
-// router.get('/', OrdersController.orders_get_all);
+//  router.get('/', checkAuth, OrdersController.orders_get_all);
     
 // router.post()
 
